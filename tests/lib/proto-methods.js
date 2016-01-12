004c8fd57ced2df3189762c00d2c3203b23bec60
LavrovArtem <artjomlav@gmail.com>
2016-01-12 17:25:15 +0300

    Update the proto-methods rule for getting rid of Array.prototype/Object.prototype methods

--- tests/lib/proto-methods.js
+++ tests/lib/proto-methods.js
@@ -9,47 +9,22 @@ var ruleTester = new RuleTester();
 
 ruleTester.run('proto-methods', rule, {
     valid: [
-        'import join, {forEach, bar as indexOf} from "../../protos";' +
-        'join(array, "");' +
-        'forEach(func);' +
-        'indexOf(str, substr);' +
-        'forEach.prop.join(array, "");' +
-        'join.toUpperCase(str);',
-
-        'import moduleName, * as unshift from "/protos";' +
-        'unshift(array);' +
-        'moduleName.prop.toLowerCase(str);' +
-        'moduleName.toLowerCase(str);',
-
-        'pop()'
+        'savedArraySlice.call(arguments);',
+        'savedObjectMethod.call(param);',
+        'savedArraySlice.apply(arguments, []);',
+        'savedObjectMethod.apply(x, [y]);'
     ],
 
     invalid: [
         {
-            code:   'import join, {forEach, bar as indexOf} from "my-module0.js";' +
-                    'arr.forEach(func);' +
-                    'str.indexOf(substr);' +
-                    'obj.join("");',
+            code:   'Array.prototype.slice.call(arguments);' +
+                    'Array.prototype.slice.apply(arguments, []);' +
+                    'Object.prototype.method.call(x);' +
+                    'Object.prototype.method.apply(x, []);',
             errors: [
                 { message: rule.ERR_MSG },
                 { message: rule.ERR_MSG },
-                { message: rule.ERR_MSG }
-            ]
-        },
-
-        {
-            code:   'import moduleName, * as unshift from "my-module1.js";' +
-                    'moduleName.func().join("");' +
-                    'obj.moduleName.join("");',
-            errors: [
                 { message: rule.ERR_MSG },
-                { message: rule.ERR_MSG }
-            ]
-        },
-
-        {
-            code:   'Array.prototype.slice.call(arguments);',
-            errors: [
                 { message: rule.ERR_MSG }
             ]
         }
